7cbd2543b1b6582874228a7d154170086ddede86
MakhachB <105069099+MakhachB@users.noreply.github.com>
2022-08-15 15:47:05 +0300

    Initial commit

---
+++ src/main/java/ru/bashirov/studenttesting/StudentTestingApplication.java
@@ -0,0 +1,13 @@
+package ru.bashirov.studenttesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bashirov/studenttesting/StudentTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bashirov.studenttesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
